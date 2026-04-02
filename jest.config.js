86250c06dd344ed08d829a2c5720e77d4de6451e
Maddy Guthridge <hello@maddyguthridge.com>
2026-04-03 01:32:41 +1100

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   maxWorkers: 1,
-
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+\\.(ts|tsx|js)$': 'ts-jest'
+    '^.+\\.(ts|tsx|js)$': ['ts-jest', {
+      useESM: true,
+    }],
   },
 };
